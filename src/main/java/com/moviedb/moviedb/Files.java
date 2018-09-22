2c369ff9d490222f061a2985c141f87ef95bb67a
niklasld <niklasld@gmail.com>
2018-09-22 12:35:26 +0200

    formatting changed to be correct in reading from files

--- src/main/java/com/moviedb/moviedb/Files.java
+++ src/main/java/com/moviedb/moviedb/Files.java
@@ -1,9 +1,12 @@
 package com.moviedb.moviedb;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Files {
 
@@ -11,13 +14,13 @@ public class Files {
     private ArrayList<Movies> movies = new ArrayList<>();
 
     public void createFile() {
-        final Formatter movies;
+        final Formatter movietxt;
 
         File movies = new File("Movies.txt");
 
         if(!movies.exists()) {
             try {
-                movies = new Formatter("Movies.txt")
+                movietxt = new Formatter("Movies.txt");
             }
             catch(Exception e) {
                 System.out.println("Error creating file Movies.txt");
@@ -39,17 +42,17 @@ public class Files {
         createFile();
         openFile();
         int movieId, year;
-        String movieTitle, link, pictureLink, genre;
-        double duration;
+        String movieTitle, link, pictureLink, genre, duration;
 
         while(scanFile.hasNext()) {
             movieId = scanFile.nextInt();
             year = scanFile.nextInt();
             movieTitle = scanFile.next();
             link = scanFile.next();
-            pictureLink = scanFile.next();
             genre = scanFile.next();
-            duration = scanFile.nextDouble();
+            duration = scanFile.next();
+            pictureLink = scanFile.next();
+
 
             movies.add(new Movies(movieId,year,movieTitle,link,genre,duration,pictureLink));
 
@@ -61,5 +64,36 @@ public class Files {
 
     public ArrayList<Movies> returnArray() {
         return movies;
+    }
+
+    public void clearFile() {
+        try {
+            FileWriter fileW = new FileWriter("Movies.txt");
+            BufferedWriter buffW = new BufferedWriter(fileW);
+            buffW.write("");
+            buffW.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeFile() {
+        try {
+            FileWriter fileW = new FileWriter("Movies.txt",true);
+            BufferedWriter buffW = new BufferedWriter(fileW);
+            clearFile();
+            for(int i = 0;i<movies.size();i++) {
+                buffW.write(""+movies.get(i).getId()+" "+movies.get(i).getYear()+" "+movies.get(i).getMovieTitle()+" "+movies.get(i).getLink()+" "+movies.get(i).getGenre()+" "+movies.get(i).getDuration()+" "+movies.get(i).getPictureLink()+""+"\n");
+            }
+            buffW.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error in writing to file Movies.txt");
+        }
+    }
+
+    public void testAddToArray() {
+        movies.add(new Movies(9,9999,"Testting_test","http://blah","Genre","22:22","Http://test.dk"));
     }
 }
--- src/main/java/com/moviedb/moviedb/Movies.java
+++ src/main/java/com/moviedb/moviedb/Movies.java
@@ -3,13 +3,13 @@ package com.moviedb.moviedb;
 public class Movies {
 
     private int id, year;
-    private String movieTitle, link, genre, pictureLink;
-    private double duration;
+    private String movieTitle, link, genre, pictureLink, duration;
+
 
     public Movies() {
     }
 
-    public Movies(int id, int year, String movieTitle, String link, String genre, double duration, String pictureLink) {
+    public Movies(int id, int year, String movieTitle, String link, String genre, String duration, String pictureLink) {
         this.id = id;
         this.year = year;
         this.movieTitle = movieTitle;
@@ -67,11 +67,11 @@ public class Movies {
         this.genre = genre;
     }
 
-    public double getDuration() {
+    public String getDuration() {
         return duration;
     }
 
-    public void setDuration(double duration) {
+    public void setDuration(String duration) {
         this.duration = duration;
     }
 
